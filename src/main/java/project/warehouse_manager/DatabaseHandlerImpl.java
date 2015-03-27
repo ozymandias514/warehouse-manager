1b6f6768afef7c9cfa8e4afaf3fee2b2fc9f842f
Fernando <jfgonzal@eagle.fgcu.edu>
2015-03-26 22:11:17 -0400

    database implementation test

--- src/main/java/project/warehouse_manager/DatabaseHandlerImpl.java
+++ src/main/java/project/warehouse_manager/DatabaseHandlerImpl.java
@@ -8,6 +8,7 @@ public class DatabaseHandlerImpl {
 	
 	public DatabaseHandlerImpl(){
 		try{
+			// database connection
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://192.185.5.78:3306/jfgonzal_test","jfgonzal_zera", "murtas12");
 			System.out.println ("Database connection established");
