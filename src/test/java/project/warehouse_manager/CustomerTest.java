b2153bf517fef0d03e26a0e0d5efe4c5a70fefe3
Carson <randomentropy1@gmail.com>
2015-03-26 21:53:51 -0400

    implemented CustomerTest.java + a few other minor changes

--- src/test/java/project/warehouse_manager/CustomerTest.java
+++ src/test/java/project/warehouse_manager/CustomerTest.java
@@ -2,68 +2,107 @@ package project.warehouse_manager;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import project.warehouse_manager.exceptions.UnitAlreadyOwnedException;
+import project.warehouse_manager.exceptions.UnitNotOwnedException;
+
 public class CustomerTest {
 
+	Customer customer;
+	String testName1;
+	String testEmail1;
+	String testName2;
+	String testEmail2;
+	
 	@Before
 	public void setUp() throws Exception {
+		testName1 = "Carson Kirkpatrick";
+		testEmail1 = "cdkirkpa@eagle.fgcu.edu";
+		testName2 = "Fernando Gonzales";
+		testEmail2 = "jfgonzal@eagle.fgcu.edu";
 	}
 
 	@Test
-	public void testCustomer() {
-		throw new RuntimeException();
-	}
-
-	@Test
-	public void testGetId() {
-		throw new RuntimeException();
-	}
-
-	@Test
-	public void testSetId() {
-		throw new RuntimeException();
+	public void testConstructor() {
+		customer = new Customer(testName1, testEmail1);
+		assertEquals(customer.getName(), testName1);
+		assertEquals(customer.getEmail(), testEmail1);
 	}
 
 	@Test
 	public void testGetName() {
-		throw new RuntimeException();
+		customer = new Customer(testName1, testEmail1);
+		assertEquals(customer.getName(), testName1);
 	}
 
 	@Test
 	public void testSetName() {
-		throw new RuntimeException();
+		customer = new Customer(testName1, testEmail1);
+		customer.setName(testName2);
+		assertEquals(customer.getName(), testName2);
+		
 	}
 
 	@Test
 	public void testGetEmail() {
-		throw new RuntimeException();
+		customer = new Customer(testName1, testEmail1);
+		assertEquals(customer.getEmail(), testEmail1);
 	}
 
 	@Test
 	public void testSetEmail() {
-		throw new RuntimeException();
+		customer = new Customer(testName1, testEmail1);
+		customer.setEmail(testEmail2);
+		assertEquals(customer.getEmail(), testEmail2);
 	}
 
 	@Test
-	public void testGetUnitsOwned() {
-		throw new RuntimeException();
+	public void testGetUnitsOwned() throws UnitAlreadyOwnedException {
+		customer = new Customer(testName1, testEmail1);
+		customer.addUnit(1);
+		assertTrue(Arrays.deepEquals(customer.getUnitsOwned(), new Integer[]{1}));
+		customer.addUnit(567);
+		assertTrue(Arrays.deepEquals(customer.getUnitsOwned(), new Integer[]{1, 567}));
 	}
 
 	@Test
-	public void testAddUnit() {
-		throw new RuntimeException();
+	public void testAddUnit() throws UnitAlreadyOwnedException {
+		customer = new Customer(testName1, testEmail1);
+		customer.addUnit(1);
+		assertTrue(customer.isUnitOwned(1));
+	}
+	
+	@Test (expected = UnitAlreadyOwnedException.class)
+	public void testAddUnitWithException() throws UnitAlreadyOwnedException {
+		customer = new Customer(testName1, testEmail1);
+		customer.addUnit(1);
+		customer.addUnit(1);
 	}
 
 	@Test
-	public void testRemoveUnit() {
-		throw new RuntimeException();
+	public void testRemoveUnit() throws UnitAlreadyOwnedException, UnitNotOwnedException {
+		customer = new Customer(testName1, testEmail1);
+		customer.addUnit(1);
+		customer.addUnit(2);
+		customer.removeUnit(1);
+		assertFalse(customer.isUnitOwned(1));
+	}
+	
+	@Test (expected = UnitNotOwnedException.class)
+	public void testRemoveUnitWithException() throws UnitNotOwnedException {
+		customer = new Customer(testName1, testEmail1);
+		customer.removeUnit(5);
 	}
 
 	@Test
-	public void testIsUnitOwned() {
-		throw new RuntimeException();
+	public void testIsUnitOwned() throws UnitAlreadyOwnedException {
+		customer = new Customer(testName1, testEmail1);
+		customer.addUnit(1);
+		assertTrue(customer.isUnitOwned(1));
 	}
 
 }
