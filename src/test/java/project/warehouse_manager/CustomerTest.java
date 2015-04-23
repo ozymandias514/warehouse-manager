4155adc8fc4f5cc7ed7a8eff8bedb80f336780b7
Carson <randomentropy1@gmail.com>
2015-04-22 23:53:23 -0400

    Patch 2.whatever.1 added some skeleton test classes for fernando

--- src/test/java/project/warehouse_manager/CustomerTest.java
+++ src/test/java/project/warehouse_manager/CustomerTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class CustomerTest {
 	Customer customer;
 	
@@ -15,10 +14,68 @@ public class CustomerTest {
 	}
 
 	@Test
-	public void testConstructor() {
-		
+	public void testCustomer() {
+//		fail("Not yet implemented");
 	}
 
-	
+	@Test
+	public void testGetDataFromCustomer() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayData() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddCustomer() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteCustomer() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeCustomerFirstName() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeCustomerLastName() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeCustomerEmail() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDataByEmail() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIdByEmail() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCustomerData() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLargeUnitsByCustomer() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSmallUnitsByCustomer() {
+//		fail("Not yet implemented");
+	}
 
 }
--- src/test/java/project/warehouse_manager/UnitTest.java
+++ src/test/java/project/warehouse_manager/UnitTest.java
@@ -6,10 +6,161 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class UnitTest {
+	Unit unit;
 	
 	@Before
 	public void setUp() throws Exception {
+		unit = new Unit();
 	}
 
-	
+	@Test
+	public void testUnit() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayLargeWarehouseData() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllLargeWarehouseUnits() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllSmallWarehouseUnits() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplaySmallWarehouseData() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetData() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmptyUnitsInLarge() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmptyUnitsInSmall() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCustomerUnitsInSmall() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCustomerUnitsInLarge() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfEmptyUnitsInLarge() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfEmptyUnitsInSmall() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfUnitsOwnedInSmallByCustomerId() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfUnitsOwnedInLargeByCustomerId() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFillUnit() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRepopulateTables() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveUnit() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTotalUnitsByCustomer() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnitsById() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangePickupDate() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeDescription() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testItemsInQueue() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNumberOfItemsInQueue() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPurgeSmall() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPurgeLarge() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncreasePriority() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangePosition() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeQueueStatus() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveToSmall() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmptyUnitByCustomer() {
+//		fail("Not yet implemented");
+	}
+
 }
